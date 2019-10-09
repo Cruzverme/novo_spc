8835771bc9ff04f4bc70c4c91ddbac7e089375b0
Cruzverme <grossmino@hotmail.com>
2019-10-09 16:51:11 -0300

    Add Views and Models

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'costumers/show'
-  get 'costumers/index'
-  get 'costumers/create'
-  get 'costumers/update'
-  get 'costumers/destroy'
-  get 'billings/create'
-  get 'billings/destroy'
-  get 'billings/update'
   devise_for :users, :controllers => { registrations: 'registrations' }
   root to: 'pages#home'
 
