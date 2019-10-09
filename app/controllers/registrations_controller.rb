40982718b994a1fcfab0431ec44edeb2a337693a
Cruzverme <grossmino@hotmail.com>
2019-10-09 17:08:49 -0300

    Create User only works with Admin

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,19 @@
 class RegistrationsController < Devise::RegistrationsController
+  before_action :authenticate_user!, :redirect_unless_admin,  only: [:new, :create]
+  skip_before_action :require_no_authentication
 
   private
+  
+  def redirect_unless_admin
+    unless current_user.try(:admin?)
+      flash[:error] = "Only admins can do that"
+      redirect_to root_path
+    end
+  end
+
+  def sign_up(resource_name, resource)
+    true
+  end
   
   def sign_up_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
