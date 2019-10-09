e5ae9fe74ca53d540b9ed5e2f16dc08491bc9f64
Cruzverme <grossmino@hotmail.com>
2019-10-09 16:37:39 -0300

    Add Admin User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_09_161509) do
+ActiveRecord::Schema.define(version: 2019_10_09_170150) do
 
   create_table "billings", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "number_titulo"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2019_10_09_161509) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
