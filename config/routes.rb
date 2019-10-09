148fd8860c524806a870e9c3fae786e963dcfa7d
Cruzverme <grossmino@hotmail.com>
2019-10-09 13:55:02 -0300

    Add NavBar Layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,4 @@
 Rails.application.routes.draw do
-  # get 'billings/create'
-  # get 'billings/destroy'
-  # get 'billings/update'
-  # get 'costumers/show'
-  # get 'costumers/index'
-  # get 'costumers/create'
-  # get 'costumers/update'
-  # get 'costumers/destroy'
-  # get 'pages/home'
 
   devise_for :users, :controllers => { registrations: 'registrations' }
   root to: 'pages#home'
