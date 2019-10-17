c3fb025ac45972c392624fb4dc077dcbc050c256
Cruzverme <grossmino@hotmail.com>
2019-10-17 14:00:25 -0300

    Add New Layout and new Action in Billings Controller

--- app/controllers/billings_controller.rb
+++ app/controllers/billings_controller.rb
@@ -1,10 +1,16 @@
+# frozen_string_literal: true
+
 class BillingsController < ApplicationController
+  def new
+    @costumer = Costumer.find_by_id(params[:costumer])
+    @billing = GetBillingCostumerService.new(@costumer.contract_number).perform
+  end
+
   def create
+    
   end
 
-  def destroy
-  end
+  def destroy; end
 
-  def update
-  end
+  def update; end
 end
