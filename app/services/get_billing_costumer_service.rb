7aa4f76a0f97a4ac782a9ce2268ba1b53fe7f378
Cruzverme <grossmino@hotmail.com>
2019-10-18 18:23:28 -0300

    Adjust Classes to unclash

--- app/services/get_billing_costumer_service.rb
+++ app/services/get_billing_costumer_service.rb
@@ -10,20 +10,23 @@ class GetBillingCostumerService
     begin
       get_billing_costumer = Rails.application.credentials[Rails.env.to_sym][:get_billing_costumer_api]
       url = "#{get_billing_costumer}?contra=#{@contract}"
-
+      
       res = RestClient.get url
       jsonParse = JSON.parse(res.body)
-
-      numeroTitulo = jsonParse["0"][0]
-      valorReal = jsonParse["0"][1]
-      vencimento = jsonParse["0"][2]
-      status_pagamento = jsonParse["0"][3]
-      valor_correcao = '%.2f' % jsonParse["0"][4]
-      success = jsonParse["success"]
-
-      boleto_maior_vencimento = [numeroTitulo,valor_correcao.to_f,vencimento,status_pagamento]
       
-      boleto_maior_vencimento
+      success = jsonParse["success"]
+      if success != 0
+        numeroTitulo = jsonParse["0"][0]
+        valorReal = jsonParse["0"][1]
+        vencimento = jsonParse["0"][2]
+        status_pagamento = jsonParse["0"][3]
+        valor_correcao = '%.2f' % jsonParse["0"][4]
+
+        boleto_maior_vencimento = [numeroTitulo,valor_correcao.to_f,vencimento,status_pagamento]
+        boleto_maior_vencimento
+      else
+        success
+      end
 
     rescue RestClient::ExceptionWithResponse => e
       e.response
--- app/services/get_clientes_erp_service.rb
+++ app/services/get_clientes_erp_service.rb
@@ -10,19 +10,21 @@ class GetClientesErpService
     begin
       get_costumer_information = Rails.application.credentials[Rails.env.to_sym][:get_costumer_info_api_url]
       url = "#{get_costumer_information}?contra=#{@contract}"
-      #url = "http://192.168.80.5/sisspc/demos/get_informacoes_contrato.php?contra=#{@contract}"
+      
       res = RestClient.get url
       jsonParse = JSON.parse(res.body)
-      
-      name = jsonParse["informacoes"]["nome"][0]
-      street = jsonParse["informacoes"]["rua"][0]
-      st_number = jsonParse["informacoes"]["numero"][0]
-      neighborhood = jsonParse["informacoes"]["bairro"][0]
       success = jsonParse["success"]
-      
-      informacoesAssinante = [name,street,st_number,neighborhood]
-      ### SE RETORNO FOR 1 ELE FOI UM SUCESSO
-      informacoesAssinante
+      if success != 0
+        name = jsonParse["informacoes"]["nome"][0]
+        street = jsonParse["informacoes"]["rua"][0]
+        st_number = jsonParse["informacoes"]["numero"][0]
+        neighborhood = jsonParse["informacoes"]["bairro"][0]
+        informacoesAssinante = [name,street,st_number,neighborhood]
+        ### SE RETORNO FOR 1 ELE FOI UM SUCESSO
+        informacoesAssinante
+      else
+        success
+      end
     rescue RestClient::ExceptionWithResponse => e
       e.response
     end
