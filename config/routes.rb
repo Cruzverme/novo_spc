b37fb387f9226e6193a3debdc6fa27e72504f443
Cruzverme <grossmino@hotmail.com>
2019-10-28 18:16:49 -0300

    Add Get Index User in Route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   root to: 'costumers#index'
   get 'costumer_informations', to: 'costumers#costumer_informations'
+  get 'users', to: 'user#index'
 
   resources :costumers
   resources :billings
